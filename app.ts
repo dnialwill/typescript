e7b123a59eeef11599d6fd9310392d7e4e5a8c20
dnialwill <dnialwill@gmail.com>
2018-07-02 08:31:47 -0400

    Added TS and NPM config, TSlint, boilerplate webpage

--- app.ts
+++ app.ts
@@ -1,1 +1,14 @@
+class Test {
+    foo: string = "bar";
 
+    /**
+     * This is a test
+     * @param bat - This is a test
+     * @param test - hey
+     */
+    bar(bat: string): string {
+        return 'baz';
+    }
+}
+
+debugger;
